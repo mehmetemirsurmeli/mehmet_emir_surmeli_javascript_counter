48e17328df79a22a2b66999e4ea4922adbce351d
mehmetemirsurmeli <mehmet.surmeli1903@gmail.com>
2023-12-06 19:13:49 +0300

    upgrade

--- app.js
+++ app.js
@@ -7,23 +7,33 @@ const btnIncrease = document.querySelector('#increase');
 
 btnDecrease.addEventListener('click', () => {
     spanValue++;
+    colorChange();
     value.textContent = spanValue;
 });
 btnReset.addEventListener('click', () => {
     spanValue = 0;
+    colorChange();
+
     value.textContent = spanValue;
 });
 btnIncrease.addEventListener('click', () => {
     spanValue--;
+    colorChange();
     value.textContent = spanValue;
 });
 
-if (value.spanValue > 0) {
-    spanValue.style.color = "green"
-} else if (value.spanValue < 0) {
-    value.style.color = "red";
-}
-else {
-    value.style.color = "black";
+
+function colorChange() {
+    if (spanValue > 0) {
+        document.getElementById("value").style.color = "green";
+
+    } else if (spanValue < 0) {
+        document.getElementById("value").style.color = "red";
+    }
+    else {
+        document.getElementById("value").style.color = "black";
+
+    }
 
 }
+console.log(document.getElementById("value"))
